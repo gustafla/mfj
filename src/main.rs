b235c2746353b5a942e7bb99fc086653575d2634
Lauri Gustafsson <me@gustafla.space>
2020-08-20 00:15:44 +0300

    Swap pico-args for argh

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
+use argh::FromArgs;
 use mfj::metadata_store::MetadataStore;
-use pico_args::Arguments;
 use std::{
     env, fs,
     path::PathBuf,
@@ -10,6 +10,32 @@ use std::{
     },
     time::Duration,
 };
+
+struct MyDuration(Duration);
+
+impl std::str::FromStr for MyDuration {
+    type Err = humantime::DurationError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        humantime::parse_duration(s).map(Self)
+    }
+}
+
+#[derive(FromArgs)]
+#[argh(description = "A telegram bot")]
+struct MfjOptions {
+    #[argh(option, description = "polling timeout in seconds", default = "60")]
+    poll_timeout: u64,
+    #[argh(
+        option,
+        description = "database file write interval (example: '30 min')",
+        default = "MyDuration(Duration::from_secs(60 * 30))"
+    )]
+    write_interval: MyDuration,
+    #[argh(switch, short = 'v', description = "log more information")]
+    verbose: bool,
+    #[argh(positional)]
+    bot_api_token: Option<String>,
+}
 
 fn find_dumps() -> Result<Vec<PathBuf>> {
     let mut entries: Vec<_> = fs::read_dir(".")?
@@ -44,31 +70,18 @@ fn main() -> Result<()> {
         Err(e) => return Err(e).context("Failed to read environment"),
     };
 
-    let mut args = Arguments::from_env();
+    let args: MfjOptions = argh::from_env();
 
-    let timeout_secs = args
-        .opt_value_from_str(["-t", "--poll-timeout"])?
-        .unwrap_or(60);
-    let write_interval = args
-        .opt_value_from_fn(["-w", "--write-interval"], |s| humantime::parse_duration(s))?
-        .unwrap_or_else(|| Duration::from_secs(60 * 30));
-    let log_level = if args.contains(["-v", "--verbose"]) {
-        log::Level::Trace
-    } else {
-        log::Level::Info
-    };
-
-    if let Some(token) = args
-        .free()
-        .context("Unexpected argument")?
-        .into_iter()
-        .next()
-        .or(var_token)
-    {
+    if let Some(token) = args.bot_api_token.as_ref().or_else(|| var_token.as_ref()) {
         let api_url = format!("https://api.telegram.org/bot{}", token);
         let default_filename = format!("./messages-{}.json.gz", chrono::Local::now());
 
-        simple_logger::init_with_level(log_level).context("Logger failed to initialize")?;
+        simple_logger::init_with_level(if args.verbose {
+            log::Level::Trace
+        } else {
+            log::Level::Info
+        })
+        .context("Logger failed to initialize")?;
         log::info!("Starting version {}", env!("CARGO_PKG_VERSION"));
 
         // TODO cleanup this
@@ -76,12 +89,13 @@ fn main() -> Result<()> {
             .iter()
             .filter_map(|read_path| {
                 log::info!("Trying to load {}", read_path.display());
-                MetadataStore::new(Some(read_path), &default_filename, write_interval).ok()
+                MetadataStore::new(Some(read_path), &default_filename, args.write_interval.0).ok()
             })
             .next()
             .unwrap_or_else(|| {
                 log::info!("Failed to load backups, starting fresh");
-                MetadataStore::new(None::<&PathBuf>, &default_filename, write_interval).unwrap()
+                MetadataStore::new(None::<&PathBuf>, &default_filename, args.write_interval.0)
+                    .unwrap()
             });
 
         let running = Arc::new(AtomicBool::new(true));
@@ -99,15 +113,19 @@ fn main() -> Result<()> {
         })
         .context("Failed to set ctrl-c handler")?;
 
-        mfj::StatsBot::new(&api_url, Duration::from_secs(timeout_secs), metadata_store)
-            .poll(running)
-            .with_context(|| {
-                format!(
-                    "{} encountered an unrecoverable error",
-                    env!("CARGO_PKG_NAME")
-                )
-            })
+        mfj::StatsBot::new(
+            &api_url,
+            Duration::from_secs(args.poll_timeout),
+            metadata_store,
+        )
+        .poll(running)
+        .with_context(|| {
+            format!(
+                "{} encountered an unrecoverable error",
+                env!("CARGO_PKG_NAME")
+            )
+        })
     } else {
-        Err(anyhow::anyhow!("Please supply a Telegram bot API_TOKEN"))
+        Err(anyhow::anyhow!("Please supply a Telegram bot API token"))
     }
 }
